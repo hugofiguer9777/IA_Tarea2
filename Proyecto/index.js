5443239eb1ef6678ba2441209e5aaf6c3b84671b
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2021-06-25 18:02:53 -0600

    Cambios index

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -243,22 +243,3 @@ function toMatriz(arreglo) {
     res.push(arreglo.slice(i, i + size));
   return res;
 }
-
-// function obtenerValorParametro(sParametroNombre) {
-//     var sPaginaURL = window.location.search.substring(1);
-//     var sURLVariables = sPaginaURL.split('&');
-//     for (var i = 0; i < sURLVariables.length; i++) {
-//       var sParametro = sURLVariables[i].split('=');
-//       if (sParametro[0] == sParametroNombre) {
-//         return sParametro[1];
-//       }
-//     }
-//     return null;
-// }
-// var turno = obtenerValorParametro('turno');
-// var estado = obtenerValorParametro('estado');
-// body = document.body.innerText = turno;
-// console.log("Turno: " + turno);
-// console.log("Estado: " + estado);
-//body=document.body.innerTex = " Estado" + estado;
-//body = document.body.innerText = 35;
